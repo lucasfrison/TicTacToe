1085e38df1ba57d71b3e24be9970cbb12a5d9d52
Lucas Frison Goncalves <lucfg15@gmail.com>
2022-06-24 12:23:14 +0000

    Replace tictactoe.h

--- tictactoe.h
+++ tictactoe.h
@@ -16,11 +16,11 @@
     
     typedef struct Velha
     {
-        int Partida; /*número da partida (Ex: primeira(1), segunda(2), terceira(3), ….,
+        int partida; /*número da partida (Ex: primeira(1), segunda(2), terceira(3), ….,
         partida)*/
-        char JogVelha[3][3]; // Tabuleiro do jogo. Armazenará as jogadas
+        char jogVelha[3][3]; // Tabuleiro do jogo. Armazenará as jogadas
         char resultado; // Resultado da Partida: X, 0 ou V(equivale a Velha - empate)
-    } partida;
+    } Partida;
     
     //FUNCOES TRABALHO 1
     extern int jogada_usuario(int linha, int coluna, char jogada);
@@ -41,8 +41,8 @@
     
     //FUNCOES NOVAS, TRABALHO 2
     extern void gerar_txt(char *nome1, char *nome2, char simbolo1, char simbolo2);
-    extern int gerar_bin(char *nome_arquivo, partida *Partida); 
-    extern partida ler_bin(char *nome_arquivo, int num_partida);
+    extern int gerar_bin(char *nome_arquivo, Partida *partida); 
+    extern Partida ler_bin(char *nome_arquivo, int num_partida);
     extern void imprime_campeonato(int MAX, char *nome_bin, char *nome_txt); //PENDENTE
     extern void limpa_buffer();
     extern void ler_nome(char *nome, char mensagem, int MAX);
